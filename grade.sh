f59bab8b46815e2a331b483cffcac0c1ce5ef63c
nathanjcho <nathan040403cho@gmail.com>
2022-11-28 03:01:02 -0800

    f

--- grade.sh
+++ grade.sh
@@ -9,14 +9,20 @@ cd student-submission
 
 if ! { -f "ListExamples.java"}; then 
     echo "ListExamples.java is either missing or incorrect, please resubmit"
-    exit 1
+    echo "You received a 0 of 3"
+    exit 
 fi 
 
-echo "file found"
-cd ..
-
-cd student-submission
+echo "File has been found"
  
-javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
-java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > grade.txt 
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java 2> compile.txt
+
+if [[ $? -ne 0 ]]
+then
+        echo "The code you submitted will not compile"
+        echo "You received a 0 of 3"
+        exit 
+fi
+
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > results.txt 
 
