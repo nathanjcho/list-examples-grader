aebc197e2cd8c7a872022ef65b489cecddce02f3
nathanjcho <nathan040403cho@gmail.com>
2022-11-28 04:06:00 -0800

    f

--- grade.sh
+++ grade.sh
@@ -1,19 +1,23 @@
 # Create your grading script here
 
-rm -rf student-submission
-git clone $1 student-submission
-echo 'Finished cloning'
+rm -rf student-submission >line3.out 2> line3.err
+echo $? > line3.exit
+git clone $1 student-submission >line5.out 2> line5.err
+echo $? > line5.exit
+echo 'Finished cloning' >line7.out 2> line7.err
+echo $? > line7.exit
 
-cd student-submission/
+cd student-submission/ 
 
 if ! [ -f "ListExamples.java" ] 
 then 
-    echo "ListExamples.java is either missing or incorrect, please resubmit"
-    echo "You received a 0 of 3"
+    echo 'ListExamples.java is either missing or incorrect, please resubmit' 
+    echo 'You received a 0 of 3' 
     exit 
 fi 
 
-echo "File has been found"
+echo "File has been found" >line15.out 2> line15.err
+echo $? > line15.exit
 cd ..
 cp TestListExamples.java student-submission/
 cd student-submission/
