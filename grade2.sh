f80b8528021a1e00f052d84bc56cabd5e0479444
nathanjcho <nathan040403cho@gmail.com>
2022-11-28 02:33:11 -0800

    f

--- grade2.sh
+++ grade2.sh
@@ -1,22 +1,31 @@
 # Create your grading script here
 
+
 rm -rf student-submission
 git clone $1 student-submission
-echo 'Finished cloning'
 
 cp TestListExamples.java student-submission
+
 cd student-submission
 
-if ! { -f "ListExamples.java"}; then 
-    echo "ListExamples.java is either missing or incorrect, please resubmit"
-    exit 1
-fi 
-
+if [[ -e ./ListExamples.java ]]
+then 
 echo "file found"
-cd ..
+fi
 
-cd student-submission
- 
 javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
 java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > grade.txt 
+#grep -w Failures grade.txt
+
+if [[ $? -eq 0 ]]
+then 
+echo "You passed!"
+else
+OUTPUT="$(grep -w Failures grade.txt)"
+echo "$OUTPUT"
+#echo "You didn't pass"
+#echo "$OUTPUT"
+echo "Your total score is" "${OUTPUT:25:1}" "out of 3"
+fi
+
 
